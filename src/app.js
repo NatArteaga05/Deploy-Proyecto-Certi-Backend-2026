3b4ba6b7978bc0f381a12676c004c2f9099611df
Nat <nataliarteaga2005@gmail.com>
2026-06-19 19:15:02 -0400

    Preparar proyecto para deploy

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import express from "express";
 import dotenv from "dotenv";
 import studentRoutes from "./routes/studentRoutes.js";
